b937d177c15234b4634cb2b53ec89427dbd8c940
Giacomo Debidda <giacomo@giacomodebidda.com>
2023-12-13 10:31:00 +0100

    update shell prompt with starship

--- programs/starship.nix
+++ programs/starship.nix
@@ -7,7 +7,7 @@
     
     # https://starship.rs/config/#prompt
     settings = {
-      add_newline = false;
+      add_newline = true; # add a blank line between shell prompts
       # https://starship.rs/config/#default-prompt-format
       format = lib.concatStrings [
         "$all"
@@ -15,6 +15,7 @@
       # https://starship.rs/config/#google-cloud-gcloud
       gcloud.disabled = true;
       jobs.disabled = true;
+      line_break.disabled = true; # make the shell prompt a single line
       os.disabled = true;
     };
   };
