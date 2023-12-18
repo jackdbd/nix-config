fda9ae818500563c292c27eb1aea589bf86dd1c8
Giacomo Debidda <giacomo@giacomodebidda.com>
2023-12-18 09:23:06 +0100

    update activitywatch Home Manager module

--- home-manager/modules/activitywatch.nix
+++ home-manager/modules/activitywatch.nix
@@ -7,12 +7,7 @@
 with lib; let
   cfg = config.services.activitywatch;
 
-  defaultActivityWatchArgs = [
-    "${pkgs.aw-qt}/bin/aw-qt"
-    # "--verbose"
-    # "--autostart-modules"
-    # comma-separated list of modules to autostart (e.g. "aw-server,aw-watcher-afk,aw-watcher-window")
-  ];
+  defaultActivityWatchArgs = ["${lib.getExe pkgs.aw-qt}"];
 
   activityWatchArgs = defaultActivityWatchArgs ++ cfg.extraOptions;
 in {
@@ -21,8 +16,16 @@ in {
   options.services.activitywatch = {
     extraOptions = mkOption {
       type = types.listOf types.str;
+      # comma-separated list of modules to autostart (e.g. "aw-server,aw-watcher-afk,aw-watcher-window")
+      # Explain whether these extraOptions override the ones defined in the TOML
+      # files (e.g. aw-qt.toml, aw-watcher-afk.toml)
       default = [];
-      example = ["--autostart-modules=aw-server,aw-watcher-afk,aw-watcher-window" "--no-gui"];
+      example = [
+        "--autostart-modules=aw-server,aw-watcher-afk,aw-watcher-window"
+        "--no-gui"
+        "--testing"
+        "--verbose"
+      ];
       description = ''
         Extra command-line arguments to pass to {command}`aw-qt`.
       '';
@@ -31,22 +34,28 @@ in {
 
   meta = {};
 
-  config.home = {
-    file = {
-      # TODO: https://github.com/jackdbd/nix-config/issues/3
-      "${config.xdg.configHome}/activitywatch/aw-client/aw-client.toml".source = ../../dotfiles/activitywatch/aw-client/aw-client.toml;
-      "${config.xdg.configHome}/activitywatch/aw-qt/aw-qt.toml".source = ../../dotfiles/activitywatch/aw-qt/aw-qt.toml;
-      "${config.xdg.configHome}/activitywatch/aw-server/aw-server.toml".source = ../../dotfiles/activitywatch/aw-server/aw-server.toml;
-      "${config.xdg.configHome}/activitywatch/aw-server-rust/config.toml".source = ../../dotfiles/activitywatch/aw-server-rust/config.toml;
-      "${config.xdg.configHome}/activitywatch/aw-watcher-afk/aw-watcher-afk.toml".source = ../../dotfiles/activitywatch/aw-watcher-afk/aw-watcher-afk.toml;
-      "${config.xdg.configHome}/activitywatch/aw-watcher-window/aw-watcher-window.toml".source = ../../dotfiles/activitywatch/aw-watcher-window/aw-watcher-window.toml;
-    };
-
-    packages = [
-      # https://github.com/NixOS/nixpkgs/tree/master/pkgs/applications/office/activitywatch
-      pkgs.activitywatch # time tracker
-    ];
+  config.home.file = {
+    # TODO: https://github.com/jackdbd/nix-config/issues/3
+    # Almost all of these TOML files are loaded by the load_config function of
+    # the aw-core library, which requires these files to be named {appname}/{appname}.toml
+    # https://github.com/ActivityWatch/aw-core
+    # aw-server-rust is an exception. It can have 2 config files:
+    # aw-server-rust/config.toml
+    # aw-server-rust/config-testing.toml
+    # https://github.com/ActivityWatch/aw-server-rust/blob/b87e32e84873793004d40649a21b49f024663a45/aw-sync/src/dirs.rs#L17
+    "${config.xdg.configHome}/activitywatch/aw-client/aw-client.toml".source = ../../dotfiles/activitywatch/aw-client/aw-client.toml;
+    "${config.xdg.configHome}/activitywatch/aw-qt/aw-qt.toml".source = ../../dotfiles/activitywatch/aw-qt/aw-qt.toml;
+    "${config.xdg.configHome}/activitywatch/aw-server/aw-server.toml".source = ../../dotfiles/activitywatch/aw-server/aw-server.toml;
+    "${config.xdg.configHome}/activitywatch/aw-server-rust/config.toml".source = ../../dotfiles/activitywatch/aw-server-rust/config.toml;
+    "${config.xdg.configHome}/activitywatch/aw-server-rust/config-testing.toml".source = ../../dotfiles/activitywatch/aw-server-rust/config-testing.toml;
+    "${config.xdg.configHome}/activitywatch/aw-watcher-afk/aw-watcher-afk.toml".source = ../../dotfiles/activitywatch/aw-watcher-afk/aw-watcher-afk.toml;
+    "${config.xdg.configHome}/activitywatch/aw-watcher-window/aw-watcher-window.toml".source = ../../dotfiles/activitywatch/aw-watcher-window/aw-watcher-window.toml;
   };
+
+  # The GUI is online at http://{address}:${port} (see aw-client/aw-client.toml and aw-server-rust/config.toml)
+
+  # https://github.com/NixOS/nixpkgs/tree/master/pkgs/applications/office/activitywatch
+  config.home.packages = [pkgs.activitywatch];
 
   # https://github.com/nix-community/home-manager/blob/e4dba0bd01956170667458be7b45f68170a63651/modules/services/syncthing.nix#L57
   # https://github.com/figsoda/cfg/blob/main/src/systemd/services/lockscreen.nix
@@ -59,8 +68,6 @@ in {
   #
   # Reload the systemd service if you make changes:
   # systemctl daemon-reload
-  #
-  # The GUI is online at http://127.0.0.1:5600/
   config.systemd.user.services.activitywatch = {
     Unit = {
       Description = "ActivityWatch - Open Source Time Tracker";
@@ -88,6 +95,6 @@ in {
     Install = {WantedBy = ["default.target"];};
   };
 
-  # TODO: launchd service for MacOS
+  # TODO: implement launchd service for MacOS
   # https://github.com/nix-community/home-manager/blob/e4dba0bd01956170667458be7b45f68170a63651/modules/services/syncthing.nix#L100C7-L100C14
 }
