bc5c4468dad779e74ec34d2729e54cd9a0df4ebe
Giacomo Debidda <giacomo@giacomodebidda.com>
2023-12-29 12:41:22 +0100

    update printing NixOS module

--- nixos/modules/printing.nix
+++ nixos/modules/printing.nix
@@ -7,27 +7,34 @@
 with lib; let
   cfg = config.services.printing;
 in {
+  meta = {};
+
   imports = [];
 
-  options = {};
-
-  meta = {};
+  options = {
+    services.printing = {
+      # already declared in nixos/modules/services/printing/cupsd.nix
+      # enable = mkEnableOption "Whether to enable printing support through the CUPS daemon.";
+    };
+  };
 
   # CUPS: configure and add network printers via http://localhost:631
   # https://nixos.wiki/wiki/Printing
-  # https://github.com/NixOS/nixpkgs/tree/master/nixos/modules/services/printing
-  # I have a Brother MFC-J6510DW
-  # https://github.com/pdewacht/brlaser
-  config.services.printing = mkIf cfg.enable {
-    # https://nixos.wiki/wiki/Printing#Manually_supplying_printer_driver
-    # pkgs.brgenml1lpr and pkgs.brgenml1cupswrapper — Generic drivers for more Brother printers
-    drivers = [pkgs.brlaser];
-  };
+  config = mkIf cfg.enable {
+    # https://nixos.wiki/wiki/Printing#Enable_autodiscovery_of_network_printers
+    services.avahi = {
+      enable = true;
+      nssmdns = true;
+      openFirewall = true;
+    };
 
-  # https://nixos.wiki/wiki/Printing#Enable_autodiscovery_of_network_printers
-  config.services.avahi = mkIf cfg.enable {
-    enable = true;
-    nssmdns = true;
-    openFirewall = true;
+    # https://github.com/NixOS/nixpkgs/tree/master/nixos/modules/services/printing
+    # I have a Brother MFC-J6510DW
+    services.printing = {
+      # https://nixos.wiki/wiki/Printing#Manually_supplying_printer_driver
+      # pkgs.brgenml1lpr and pkgs.brgenml1cupswrapper — Generic drivers for more Brother printers
+      # https://github.com/pdewacht/brlaser
+      drivers = [pkgs.brlaser];
+    };
   };
 }
