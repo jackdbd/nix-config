5e3b89b35ec056ce6d6c46e466f3a01e4c1297cc
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-01-06 10:10:32 +0100

    add a few VS Code extensions

--- home-manager/modules/vscode/default.nix
+++ home-manager/modules/vscode/default.nix
@@ -3,101 +3,8 @@
   lib,
   pkgs,
   ...
-}:
-with lib; let
-  inherit (pkgs.vscode-utils) buildVscodeMarketplaceExtension;
-
+}: let
   cfg = config.programs.vscode;
-
-  activitywatch.aw-watcher-vscode = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "aw-watcher-vscode";
-      publisher = "ActivityWatch";
-      version = "0.5.0";
-      # sha256 = lib.fakeSha256;
-      sha256 = "sha256-OrdIhgNXpEbLXYVJAx/jpt2c6Qa5jf8FNxqrbu5FfFs=";
-    };
-    meta = {
-      changelog = "https://marketplace.visualstudio.com/items/activitywatch.aw-watcher-vscode/changelog";
-      downloadPage = "https://marketplace.visualstudio.com/items?itemName=activitywatch.aw-watcher-vscode";
-      homepage = "https://github.com/ActivityWatch/aw-watcher-vscode#readme";
-      license = lib.licenses.mpl20;
-    };
-  };
-
-  bierner.color-info = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "color-info";
-      publisher = "bierner"; # Matt Bierner
-      version = "0.7.2";
-      sha256 = "sha256-Bf0thdt4yxH7OsRhIXeqvaxD1tbHTrUc4QJcju7Hv90=";
-    };
-    meta = {
-      changelog = "https://marketplace.visualstudio.com/items/bierner.color-info/changelog";
-      downloadPage = "https://marketplace.visualstudio.com/items?itemName=bierner.color-info";
-      homepage = "https://github.com/mattbierner/vscode-color-info#readme";
-      license = lib.licenses.mit;
-    };
-  };
-
-  chakrounanas.turbo-console-log = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "turbo-console-log";
-      publisher = "ChakrounAnas";
-      version = "2.9.10";
-      sha256 = "sha256-1QyDdE/M7IJ9WByhCUNCneThc7UZ+4B+B2pRwDYy7K0=";
-    };
-    meta = {
-      changelog = "https://marketplace.visualstudio.com/items/ChakrounAnas.turbo-console-log/changelog";
-      downloadPage = "https://marketplace.visualstudio.com/items?itemName=ChakrounAnas.turbo-console-log";
-      homepage = "https://github.com/Chakroun-Anas/turbo-console-log#readme";
-      license = lib.licenses.mit;
-    };
-  };
-
-  dtsvet.vscode-wasm = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "vscode-wasm";
-      publisher = "dtsvet"; # WebAssembly Foundation
-      version = "1.4.1";
-      sha256 = "sha256-zs7E3pxf4P8kb3J+5zLoAO2dvTeepuCuBJi5s354k0I=";
-    };
-    meta = {
-      changelog = "https://marketplace.visualstudio.com/items/dtsvet.vscode-wasm/changelog";
-      downloadPage = "https://marketplace.visualstudio.com/items?itemName=dtsvet.vscode-wasm";
-      homepage = "https://github.com/wasmerio/vscode-wasm";
-      license = lib.licenses.mit;
-    };
-  };
-
-  stylelint.vscode-stylelint = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "vscode-stylelint";
-      publisher = "Stylelint";
-      version = "1.3.0";
-      sha256 = "sha256-JoCa2d0ayBEuCcQi3Z/90GJ4AIECVz8NCpd+i+9uMeA=";
-    };
-    meta = {
-      changelog = "https://marketplace.visualstudio.com/items/stylelint.vscode-stylelint/changelog";
-      downloadPage = "https://marketplace.visualstudio.com/items?itemName=stylelint.vscode-stylelint";
-      homepage = "https://github.com/stylelint/vscode-stylelint#readme";
-      license = lib.licenses.mit;
-    };
-  };
-
-  yoavbls.pretty-ts-errors = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "pretty-ts-errors";
-      publisher = "yoavbls";
-      version = "0.5.2";
-      sha256 = "sha256-g6JIiXfjQKQEtdXZgsQsluKuJZO0MsD1ijy+QLYE1uY=";
-    };
-    meta = {
-      downloadPage = "https://marketplace.visualstudio.com/items?itemName=yoavbls.pretty-ts-errors";
-      homepage = "https://github.com/yoavbls/pretty-ts-errors#readme";
-      license = lib.licenses.mit;
-    };
-  };
 in {
   meta = {};
 
@@ -118,50 +25,48 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     home.packages = [cfg.package];
 
     # https://mipmip.github.io/home-manager-option-search/?query=vscode
     programs.vscode = {
       enableExtensionUpdateCheck = true;
       enableUpdateCheck = false;
-
-      # Do NOT declare VS Code extensions like this:
-      # extensions = [ "esbenp.prettier-vscode" ];
+      # with pkgs.vscode-extensions;
+      # Do NOT declare VS Code extensions using a string: "esbenp.prettier-vscode"
       # Instead, search `vscode-extensions` on NixOS Search.
       # https://search.nixos.org/packages
       # https://github.com/NixOS/nixpkgs/tree/master/pkgs/applications/editors/vscode
       # VS Code themes
       # https://mynixos.com/search?q=vscode+theme
-      extensions = with pkgs.vscode-extensions; [
-        activitywatch.aw-watcher-vscode
-        bbenoist.nix # Nix language support for VS Code
-        betterthantomorrow.calva # Clojure/Script interactive programming for VS Code
-        bierner.color-info # Provides quick information about css colors
-        chakrounanas.turbo-console-log
-        christian-kohler.path-intellisense
-        coolbear.systemd-unit-file # syntax highlighting for systemd unit files
-        davidanson.vscode-markdownlint
-        dtsvet.vscode-wasm # WebAssembly Toolkit for VSCode
-        eamodio.gitlens
-        esbenp.prettier-vscode
-        github.copilot
-        github.copilot-chat
-        github.vscode-github-actions
-        github.vscode-pull-request-github
-        jdinhlife.gruvbox # Gruvbox theme
-        jnoortheen.nix-ide # Nix language support with formatting and error report
-        kamadorueda.alejandra # Nix formatter
-        mechatroner.rainbow-csv
-        mhutchie.git-graph
-        ms-azuretools.vscode-docker
-        # at the moment mtxr.sqltools is not available on nixpkgs (not even on
-        # nixpkgs unstable) https://github.com/mtxr/vscode-sqltools
-        stylelint.vscode-stylelint
-        tamasfe.even-better-toml
-        usernamehw.errorlens
-        yoavbls.pretty-ts-errors
-      ];
+      extensions = with pkgs.vscode-extensions;
+        [
+          bbenoist.nix # Nix language support for VS Code
+          betterthantomorrow.calva # Clojure/Script interactive programming for VS Code
+          bradlc.vscode-tailwindcss # Tailwind CSS tooling for VS Code
+          christian-kohler.path-intellisense
+          coolbear.systemd-unit-file # syntax highlighting for systemd unit files
+          davidanson.vscode-markdownlint
+          dbaeumer.vscode-eslint # integrates ESLint into VS Code
+          eamodio.gitlens
+          esbenp.prettier-vscode
+          gencer.html-slim-scss-css-class-completion # CSS class name completion
+          github.copilot
+          github.copilot-chat
+          github.vscode-github-actions
+          github.vscode-pull-request-github
+          jdinhlife.gruvbox # Gruvbox theme
+          jnoortheen.nix-ide # Nix language support with formatting and error report
+          kamadorueda.alejandra # Nix formatter
+          mechatroner.rainbow-csv
+          mhutchie.git-graph
+          ms-azuretools.vscode-docker
+          # at the moment mtxr.sqltools is not available on nixpkgs (not even on
+          # nixpkgs unstable) https://github.com/mtxr/vscode-sqltools
+          tamasfe.even-better-toml
+          usernamehw.errorlens
+        ]
+        ++ import ./extra-vscode-extensions.nix {inherit lib pkgs;};
 
       # https://github.com/jackdbd/dotfiles/tree/main/Code/.config/Code/User/snippets
       globalSnippets = import ./global-snippets.nix;
---
+++ home-manager/modules/vscode/extra-vscode-extensions.nix
@@ -0,0 +1,169 @@
+{
+  lib,
+  pkgs,
+}: let
+  inherit (pkgs.vscode-utils) buildVscodeMarketplaceExtension;
+
+  activitywatch.aw-watcher-vscode = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "aw-watcher-vscode";
+      publisher = "ActivityWatch";
+      version = "0.5.0";
+      # sha256 = lib.fakeSha256;
+      sha256 = "sha256-OrdIhgNXpEbLXYVJAx/jpt2c6Qa5jf8FNxqrbu5FfFs=";
+    };
+    meta = {
+      changelog = "https://marketplace.visualstudio.com/items/activitywatch.aw-watcher-vscode/changelog";
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=activitywatch.aw-watcher-vscode";
+      homepage = "https://github.com/ActivityWatch/aw-watcher-vscode#readme";
+      license = lib.licenses.mpl20;
+    };
+  };
+
+  bierner.color-info = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "color-info";
+      publisher = "bierner"; # Matt Bierner
+      version = "0.7.2";
+      sha256 = "sha256-Bf0thdt4yxH7OsRhIXeqvaxD1tbHTrUc4QJcju7Hv90=";
+    };
+    meta = {
+      changelog = "https://marketplace.visualstudio.com/items/bierner.color-info/changelog";
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=bierner.color-info";
+      homepage = "https://github.com/mattbierner/vscode-color-info#readme";
+      license = lib.licenses.mit;
+    };
+  };
+
+  chakrounanas.turbo-console-log = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "turbo-console-log";
+      publisher = "ChakrounAnas";
+      version = "2.9.10";
+      sha256 = "sha256-1QyDdE/M7IJ9WByhCUNCneThc7UZ+4B+B2pRwDYy7K0=";
+    };
+    meta = {
+      changelog = "https://marketplace.visualstudio.com/items/ChakrounAnas.turbo-console-log/changelog";
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=ChakrounAnas.turbo-console-log";
+      homepage = "https://github.com/Chakroun-Anas/turbo-console-log#readme";
+      license = lib.licenses.mit;
+    };
+  };
+
+  dtsvet.vscode-wasm = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "vscode-wasm";
+      publisher = "dtsvet"; # WebAssembly Foundation
+      version = "1.4.1";
+      sha256 = "sha256-zs7E3pxf4P8kb3J+5zLoAO2dvTeepuCuBJi5s354k0I=";
+    };
+    meta = {
+      changelog = "https://marketplace.visualstudio.com/items/dtsvet.vscode-wasm/changelog";
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=dtsvet.vscode-wasm";
+      homepage = "https://github.com/wasmerio/vscode-wasm";
+      license = lib.licenses.mit;
+    };
+  };
+
+  pflannery.vscode-versionlens = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "vscode-versionlens";
+      publisher = "pflannery";
+      version = "1.9.2";
+      sha256 = "sha256-2wmdfvD6j/gQ3IEAn1vKOOzbqlmbWhVM5qXlLoBmg3c=";
+    };
+    meta = {
+      changelog = "https://marketplace.visualstudio.com/items/pflannery.vscode-versionlens/changelog";
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=pflannery.vscode-versionlens";
+      homepage = "https://gitlab.com/versionlens/vscode-versionlens#readme";
+      license = lib.licenses.mit;
+    };
+  };
+
+  pranaygp.vscode-css-peek = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "vscode-css-peek";
+      publisher = "pranaygp";
+      version = "4.4.1";
+      sha256 = "sha256-GX6J9DfIW9CLarSCfWhJQ9vvfUxy8QU0kh3cfRUZIaE=";
+    };
+    meta = {
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=pranaygp.vscode-css-peek";
+      homepage = "https://github.com/pranaygp/vscode-css-peek/blob/master/README.md";
+      license = lib.licenses.mit;
+    };
+  };
+
+  ryan-heybourn.headwind = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "headwind";
+      publisher = "heybourn";
+      version = "1.7.0";
+      sha256 = "sha256-yXsZoSuJQTdbHLjEERXX2zVheqNYmcPXs97/uQYa7og=";
+    };
+    meta = {
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=heybourn.headwind";
+      homepage = "https://github.com/heybourn/headwind#readme";
+      license = lib.licenses.mit;
+    };
+  };
+
+  stylelint.vscode-stylelint = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "vscode-stylelint";
+      publisher = "Stylelint";
+      version = "1.3.0";
+      sha256 = "sha256-JoCa2d0ayBEuCcQi3Z/90GJ4AIECVz8NCpd+i+9uMeA=";
+    };
+    meta = {
+      changelog = "https://marketplace.visualstudio.com/items/stylelint.vscode-stylelint/changelog";
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=stylelint.vscode-stylelint";
+      homepage = "https://github.com/stylelint/vscode-stylelint#readme";
+      license = lib.licenses.mit;
+    };
+  };
+
+  yoavbls.pretty-ts-errors = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "pretty-ts-errors";
+      publisher = "yoavbls";
+      version = "0.5.2";
+      sha256 = "sha256-g6JIiXfjQKQEtdXZgsQsluKuJZO0MsD1ijy+QLYE1uY=";
+    };
+    meta = {
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=yoavbls.pretty-ts-errors";
+      homepage = "https://github.com/yoavbls/pretty-ts-errors#readme";
+      license = lib.licenses.mit;
+    };
+  };
+
+  webhint.vscode-webhint = buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "vscode-webhint";
+      publisher = "webhint";
+      version = "2.1.10";
+      sha256 = "sha256-I+mDpMb9SgGeh6+a2JePC98ZXZyU+WztmwyX4dWtySY="; # v2.1.10
+      # Version 2.1.11 doesn't work. Nix is able to build it, but it doesn't install it in VS Code.
+      # version = "2.1.11";
+      # sha256 = "sha256-Ir+Dk63k+eE+1PKQnO9FkCEXIVcfj/p13RcwTOf7qdM="; # v2.1.11
+    };
+    meta = {
+      changelog = "https://marketplace.visualstudio.com/items/webhint.vscode-webhint/changelog";
+      downloadPage = "https://marketplace.visualstudio.com/items?itemName=webhint.vscode-webhint";
+      homepage = "https://webhint.io/";
+      license = lib.licenses.asl20; # Apache License 2.0
+    };
+  };
+in
+  with pkgs.vscode-extensions; [
+    activitywatch.aw-watcher-vscode
+    bierner.color-info # Provides quick information about css colors
+    chakrounanas.turbo-console-log
+    dtsvet.vscode-wasm # WebAssembly Toolkit for VSCode
+    pflannery.vscode-versionlens
+    pranaygp.vscode-css-peek
+    ryan-heybourn.headwind
+    stylelint.vscode-stylelint
+    yoavbls.pretty-ts-errors
+    webhint.vscode-webhint
+  ]
