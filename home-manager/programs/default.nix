cbe213b8f9a0ab4f339bd340423034e8907d34d8
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-01-06 22:18:43 +0100

    add btop; add theme for bat

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -5,11 +5,31 @@ let
     ...
   }: {
     programs = {
-      bat.enable = true;
+      bat = {
+        enable = true;
+        config = {
+          # https://github.com/sharkdp/bat?tab=readme-ov-file#customization
+          # theme = "Dracula";
+          theme = "Monokai Extended Origin";
+          # theme = "TwoDark";
+        };
+      };
 
       broot.enable = true;
 
-      htop.enable = true;
+      btop = {
+        enable = true;
+        settings = {
+          color_theme = "Default";
+        };
+      };
+
+      htop = {
+        enable = true;
+        settings = {
+          color_scheme = 6;
+        };
+      };
 
       tealdeer = {
         enable = true;
