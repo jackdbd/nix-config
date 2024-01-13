c9d9df9630124f83b74587211632ab936f72e315
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-01-14 00:05:42 +0100

    add udev rules for Android

--- nixos/modules/android.nix
+++ nixos/modules/android.nix
@@ -13,10 +13,21 @@
   config = {
     environment.systemPackages = with pkgs; [
       # android-backup-extractor
+
       # android-studio-stable
-      android-tools # Android SDK platform tools (adb, fastboot, etc...)
-      apktool # A tool for reverse engineering Android apk files
-      trueseeing # Non-decompiling Android vulnerability scanner
+
+      # Android SDK platform tools (adb, fastboot, etc...)
+      android-tools
+
+      # To run apps on an Android device, the computer needs to have installed
+      # the udev rules that cover that Android device
+      android-udev-rules
+
+      # A tool for reverse engineering Android apk files
+      apktool
+
+      # Non-decompiling Android vulnerability scanner
+      trueseeing
     ];
   };
 }
