1fed3da23da26a15d439733ce0308d587e88ed27
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-05-02 15:15:09 +0200

    remove trezor_agent and unset GNUPGHOME

--- nixos/modules/trezor.nix
+++ nixos/modules/trezor.nix
@@ -20,20 +20,27 @@ in {
     };
   };
 
+  # TODO: the overall support for hardware devices like the Trezor does not seem great among email clients:
+  # - https://github.com/openpgpjs/openpgpjs/issues/362
+  # - https://github.com/FlowCrypt/flowcrypt-browser/issues/964
+  # - https://github.com/ProtonMail/WebClients/issues/122
+  # So, for the time being I have decided NOT to store my private OpenPGP key on the Trezor.
+  # If and when I decide to change my mind about this, I will enable trezor_agent and set the GNUPGHOME environment variable.
+
   # https://nixos.org/manual/nixos/stable/#trezor
   # https://mynixos.com/nixpkgs/package/trezord
   # https://docs.trezor.io/trezor-user-env/development.html
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       trezord
-      trezor_agent # hardware-based agent for SSH/OpenPGP/age keys
+      # trezor_agent # hardware-based agent for SSH/OpenPGP/age keys
     ];
 
     environment.sessionVariables = {
       # This tells trezor-agent that all my private keys are backed by my Trezor.
       # Unset this environment variable if you wish to switch back to your software keys.
       # https://github.com/romanz/trezor-agent/blob/master/doc/README-GPG.md
-      GNUPGHOME = "$HOME/.gnupg/trezor";
+      # GNUPGHOME = "$HOME/.gnupg/trezor";
     };
   };
 }
