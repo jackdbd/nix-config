0f7f29892ffdfaf17200c72f121e95148341dd84
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-06-21 12:26:23 +0200

    update tarsnap config

--- nixos/modules/tarsnap.nix
+++ nixos/modules/tarsnap.nix
@@ -2,10 +2,11 @@
   config,
   lib,
   pkgs,
+  user,
   ...
 }:
 with lib; let
-  cfg = config.services.trezord;
+  cfg = config.services.tarsnap;
 in {
   meta = {};
 
@@ -25,10 +26,39 @@ in {
       tarsnap
     ];
 
+    # Visit https://www.tarsnap.com/account.html to manage your Tarsnap account
+
+    # List all Tarsnap archives available on this machine
+    # tarsnap --list-archives --keyfile "$HOME/tarsnap.key" | sort
+
+    # Restore a Tarsnap archive available on this machine
+    # tarsnap -x -f mycomputer-2015-08-09_19-37-20 --keyfile "$HOME/tarsnap.key"
+
     # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/backup/tarsnap.nix
     services.tarsnap = {
-      archives = {};
-      keyfile = "";
+      # This creates the systemd system timer tarsnap-docs.timer, which will
+      # trigger the systemd system service tarsnap-docs.service
+      # You can list all systemd system timers with this command:
+      # systemctl --system list-timers
+      archives.docs = {
+        directories = [
+          "/home/${user}/Documents/shared-documents"
+        ];
+        excludes = [];
+        nodump = true;
+        # period is described in man systemd.time. Examples: "01:15", "hourly", "daily"
+        period = "daily";
+        printStats = true;
+        verbose = true;
+      };
+
+      archives.music = {
+        directories = [
+          "/home/${user}/Music/shared-music/random-mix"
+        ];
+      };
+
+      keyfile = "/home/${user}/tarsnap.key";
     };
   };
 }
