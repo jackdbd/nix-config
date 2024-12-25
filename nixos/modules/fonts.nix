686bed76f145c00e23cae84408319c156704bb56
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-12-25 13:38:34 +0100

    update fonts

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -11,26 +11,26 @@ with lib; {
 
   options = {};
 
-  # config.environment.systemPackages = with pkgs; [
-  #   nerdfonts
-  # ];
-
   config.fonts = {
-    # Enable all fonts from this list:
-    # https://github.com/NixOS/nixpkgs/blob/d02d818f22c777aa4e854efc3242ec451e5d462a/nixos/modules/config/fonts/packages.nix#L34
     enableDefaultPackages = true;
     packages = with pkgs; [
       # Enable a few fonts from this list:
       # https://github.com/NixOS/nixpkgs/tree/master/pkgs/data/fonts
       alegreya
+      dina-font
+      fira-code # https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/fira-code/default.nix
+      fira-code-symbols # https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/fira-code/symbols.nix
+      iosevka # https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/iosevka/default.nix
+      liberation_ttf
+      mplus-outline-fonts.githubRelease
       # NixOS contains only this subset of the entire Nerd Fonts package.
       # Here we select only a subset of that subset.
       # https://nixos.wiki/wiki/Fonts#Installing_only_specific_nerdfonts
       nerd-fonts.droid-sans-mono
-      nerd-fonts.fira-code
       nerd-fonts.jetbrains-mono
       nerd-fonts.space-mono
       noto-fonts
+      noto-fonts-color-emoji
       noto-fonts-cjk-sans # Chinese, Japanese, Korean
       noto-fonts-emoji # color emojis
       source-code-pro
