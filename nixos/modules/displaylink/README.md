62b7e6549123369d56e0d6d38a56353f0dbc7e81
Giacomo Debidda <giacomo@giacomodebidda.com>
2025-11-05 07:44:47 +0100

    update displaylink NixOS module (external monitor still not working)

--- nixos/modules/displaylink/README.md
+++ nixos/modules/displaylink/README.md
@@ -5,3 +5,7 @@ Download the DisplayLink driver for Linux:
 ```sh
 nix-prefetch-url --name displaylink-620.zip https://www.synaptics.com/sites/default/files/exe_files/2025-09/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.2-EXE.zip
 ```
+
+## References
+
+- [DisplayLink (NixOS Wiki)](https://wiki.nixos.org/wiki/Displaylink)
--- nixos/modules/displaylink/default.nix
+++ nixos/modules/displaylink/default.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   meta = {};
 
   imports = [];
@@ -6,11 +11,28 @@
   options = {};
 
   config = {
+    boot = {
+      extraModulePackages = [config.boot.kernelPackages.evdi];
+      initrd.kernelModules = ["evdi"];
+    };
+
     environment.systemPackages = with pkgs; [
       displaylink
     ];
+
     services.xserver = {
+      displayManager.sessionCommands = ''
+        ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
+      '';
       videoDrivers = ["displaylink" "modesetting"];
     };
+
+    # Enable the DisplayLink Manager Service
+    # The service name might vary, 'dlm' or 'displaylink-server' are common.
+    # Check the NixOS wiki for the exact name in your version.
+    # Using 'dlm' is a common choice for the provided NixOS package.
+    systemd.services.dlm.wantedBy = ["multi-user.target"];
+    # OR for the server name:
+    # systemd.services.displaylink-server.wantedBy = [ "multi-user.target" ];
   };
 }
